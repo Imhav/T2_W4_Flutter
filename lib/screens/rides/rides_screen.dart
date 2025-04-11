236a0e14499434fb39db28637d4870f4c0025e3d
Imhav <ouvenhav500@gmail.com>
2025-04-11 18:15:21 +0700

    BLA-105 – Handle the Preference edition on Ride Screen

--- lib/screens/rides/rides_screen.dart
+++ lib/screens/rides/rides_screen.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:week_3_blabla_project/screens/rides/widgets/ride_pref_bar.dart';
- 
-import '../../dummy_data/dummy_data.dart';
-import '../../model/ride/ride.dart';
-import '../../model/ride_pref/ride_pref.dart';
-import '../../service/rides_service.dart';
-import '../../theme/theme.dart';
- 
+import 'package:w4_flutter/model/ride/ride.dart';
+import 'package:w4_flutter/model/ride_pref/ride_pref.dart';
+import 'package:w4_flutter/service/ride_prefs_service.dart';
+import 'package:w4_flutter/service/rides_service.dart';
+import 'package:w4_flutter/theme/theme.dart';
+import 'package:w4_flutter/utils/animations_util.dart';
+import '../../model/ride/locations.dart';
+import 'widgets/ride_pref_bar.dart';
+import 'widgets/ride_pref_modal.dart';
 import 'widgets/rides_tile.dart';
 
-///
-///  The Ride Selection screen allow user to select a ride, once ride preferences have been defined.
-///  The screen also allow user to re-define the ride preferences and to activate some filters.
-///
 class RidesScreen extends StatefulWidget {
   const RidesScreen({super.key});
 
@@ -21,52 +18,173 @@ class RidesScreen extends StatefulWidget {
 }
 
 class _RidesScreenState extends State<RidesScreen> {
- 
-  RidePreference currentPreference  = fakeRidePrefs[0];   // TODO 1 :  We should get it from the service
+  late RidePreference currentPreference;
 
-  List<Ride> get matchingRides => RidesService.getRidesFor(currentPreference);
+  @override
+  void initState() {
+    super.initState();
+    currentPreference =
+        RidePrefService.instance.currentPreference ??
+        RidePreference(
+          departure: Location(name: "", country: Country.cambodia),
+          arrival: Location(name: "", country: Country.cambodia),
+          departureDate: DateTime.now(),
+          requestedSeats: 1,
+        );
+  }
+
+  List<Ride> get matchingRides =>
+      RidesService.instance.repository.getRides(currentPreference, null);
 
   void onBackPressed() {
-    Navigator.of(context).pop();     //  Back to the previous view
-  } 
+    Navigator.of(context).pop();
+  }
 
   void onPreferencePressed() async {
-        // TODO  6 : we should push the modal with the current pref
+    RidePreference? newPref = await Navigator.of(context).push<RidePreference>(
+      AnimationUtils.createTopSheetRoute(
+        RidePrefModal(currentPreference: currentPreference),
+      ),
+    );
 
-        // TODO 9 :  After pop, we should get the new current pref from the modal 
-
-        // TODO 10 :  Then we should update the service current pref,   and update the view
+    if (newPref != null) {
+      RidePrefService.instance.setCurrentPreference(newPref);
+      setState(() {
+        currentPreference = newPref;
+      });
+    }
   }
 
   void onFilterPressed() {
+    // TODO: Implement filter logic (e.g., pet filter) if needed
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.only(
-          left: BlaSpacings.m, right: BlaSpacings.m, top: BlaSpacings.s),
-      child: Column(
-        children: [
-          // Top search Search bar
-          RidePrefBar(
-              ridePreference: currentPreference,
-              onBackPressed: onBackPressed,
-              onPreferencePressed: onPreferencePressed,
-              onFilterPressed: onFilterPressed),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: matchingRides.length,
-              itemBuilder: (ctx, index) => RideTile(
-                ride: matchingRides[index],
-                onPressed: () {},
-              ),
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: BlaSpacings.m,
+            right: BlaSpacings.m,
+            top: BlaSpacings.s,
           ),
-        ],
+          child: Column(
+            children: [
+              RidePrefBar(
+                ridePreference: currentPreference,
+                onBackPressed: onBackPressed,
+                onPreferencePressed: onPreferencePressed,
+                onFilterPressed: onFilterPressed,
+              ),
+              ListView.builder(
+                physics:
+                    const NeverScrollableScrollPhysics(), // Prevent nested scrolling
+                shrinkWrap: true, // Fit content within SingleChildScrollView
+                itemCount: matchingRides.length,
+                itemBuilder:
+                    (ctx, index) =>
+                        RideTile(ride: matchingRides[index], onPressed: () {}),
+              ),
+            ],
+          ),
+        ),
       ),
-    ));
+    );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// import '../../model/ride/ride.dart';
+
+// import '../../model/ride_pref/ride_pref.dart';
+// import '../../service/ride_prefs_service.dart';
+// import '../../service/rides_service.dart';
+// import '../../theme/theme.dart';
+// import '../../utils/animations_util.dart';
+// import 'widgets/ride_pref_bar.dart';
+// import 'widgets/ride_pref_modal.dart';
+// import 'widgets/rides_tile.dart';
+
+// ///
+// ///  The Ride Selection screen allow user to select a ride, once ride preferences have been defined.
+// ///  The screen also allow user to re-define the ride preferences and to activate some filters.
+// ///
+// class RidesScreen extends StatefulWidget {
+//   const RidesScreen({super.key});
+
+//   @override
+//   State<RidesScreen> createState() => _RidesScreenState();
+// }
+
+// class _RidesScreenState extends State<RidesScreen> {
+//   RidePreference get currentPreference =>
+//       RidePrefService.instance.currentPreference!;
+
+//   RidesFilter currentFilter = RidesFilter();
+
+//   List<Ride> get matchingRides =>
+//       RidesService.instance.getRides(currentPreference, currentFilter);
+
+//   void onBackPressed() {
+//     // 1 - Back to the previous view
+//     Navigator.of(context).pop();
+//   }
+
+//   onRidePrefSelected(RidePreference newPreference) async {}
+
+//   void onPreferencePressed() async {
+//     // Open a modal to edit the ride preferences
+//     RidePreference? newPreference = await Navigator.of(
+//       context,
+//     ).push<RidePreference>(
+//       AnimationUtils.createTopToBottomRoute(
+//         RidePrefModal(initialPreference: currentPreference),
+//       ),
+//     );
+
+//     if (newPreference != null) {
+//       // 1 - Update the current preference
+//       RidePrefService.instance.setCurrentPreference(newPreference);
+
+//       // 2 -   Update the state   -- TODO MAKE IT WITH STATE MANAGEMENT
+//       setState(() {});
+//     }
+//   }
+
+//   void onFilterPressed() {}
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Padding(
+//         padding: const EdgeInsets.only(
+//           left: BlaSpacings.m,
+//           right: BlaSpacings.m,
+//           top: BlaSpacings.s,
+//         ),
+//         child: Column(
+//           children: [
+//             // Top search Search bar
+//             RidePrefBar(
+//               ridePreference: currentPreference,
+//               onBackPressed: onBackPressed,
+//               onPreferencePressed: onPreferencePressed,
+//               onFilterPressed: onFilterPressed,
+//             ),
+
+//             Expanded(
+//               child: ListView.builder(
+//                 itemCount: matchingRides.length,
+//                 itemBuilder:
+//                     (ctx, index) =>
+//                         RideTile(ride: matchingRides[index], onPressed: () {}),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/screens/rides/widgets/ride_pref_modal.dart
+++ lib/screens/rides/widgets/ride_pref_modal.dart
@@ -1,62 +1,126 @@
 import 'package:flutter/material.dart';
-import 'package:week_3_blabla_project/widgets/actions/bla_icon_button.dart';
-
-import '../../../model/ride_pref/ride_pref.dart';
-import '../../../theme/theme.dart';
+import 'package:w4_flutter/model/ride_pref/ride_pref.dart';
+import 'package:w4_flutter/service/ride_prefs_service.dart';
+import 'package:w4_flutter/theme/theme.dart';
+import 'package:w4_flutter/widgets/actions/bla_icon_button.dart';
 import '../../ride_pref/widgets/ride_pref_form.dart';
 
 class RidePrefModal extends StatefulWidget {
+  RidePreference? currentPreference; // Make optional to handle null
+  RidePrefModal({super.key, required this.currentPreference});
 
-  const RidePrefModal({
-
-    super.key,     
-     // TODO 7 : We should pass the current prefs to this moda; 
-  });
- 
   @override
   State<RidePrefModal> createState() => _RidePrefModalState();
 }
 
 class _RidePrefModalState extends State<RidePrefModal> {
   void onBackSelected() {
-    Navigator.of(context).pop();
+    Navigator.of(context).pop(); // Close modal without changes
   }
 
   void onSubmit(RidePreference newPreference) {
-      // TODO 9 : We should pop this modal, with the new current preference
+    // TODO 9: Pop the modal with the new current preference
+    Navigator.of(context).pop(newPreference); // Return the new preference
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: const EdgeInsets.only(
-          left: BlaSpacings.m, right: BlaSpacings.m, top: BlaSpacings.s),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Back icon
-          BlaIconButton(
-            onPressed: onBackSelected,
-            icon: Icons.close,
-          ),
-          SizedBox(height: BlaSpacings.m),
+      body: Padding(
+        padding: const EdgeInsets.only(
+          left: BlaSpacings.m,
+          right: BlaSpacings.m,
+          top: BlaSpacings.s,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Back icon
+            BlaIconButton(onPressed: onBackSelected, icon: Icons.close),
+            SizedBox(height: BlaSpacings.m),
 
-          // Title
-          Text("Edit your search",
-              style: BlaTextStyles.title.copyWith(color: BlaColors.textNormal)),
-
-          // Form
-          Expanded(
-              child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: RidePrefForm(
-              initialPreference: null,        // TODO 7 : The form should be displayed with the modal current prefs
-              onSubmit: onSubmit,
+            // Title
+            Text(
+              "Edit your search",
+              style: BlaTextStyles.title.copyWith(color: BlaColors.textNormal),
             ),
-          )),
-        ],
+
+            // Form
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: RidePrefForm(
+                  initialPreference:
+                      RidePrefService
+                          .instance
+                          .currentPreference, // TODO 7 : The form should be displayed with the modal current prefs
+                  onSubmit: onSubmit,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+
+// import 'package:flutter/material.dart';
+
+// import '../../../model/ride_pref/ride_pref.dart';
+// import '../../../theme/theme.dart';
+// import '../../../widgets/actions/bla_icon_button.dart';
+// import '../../ride_pref/widgets/ride_pref_form.dart';
+
+// class RidePrefModal extends StatefulWidget {
+//   const RidePrefModal({super.key, required this.initialPreference});
+
+//   final RidePreference? initialPreference;
+
+//   @override
+//   State<RidePrefModal> createState() => _RidePrefModalState();
+// }
+
+// class _RidePrefModalState extends State<RidePrefModal> {
+//   void onBackSelected() {
+//     Navigator.of(context).pop();
+//   }
+
+//   void onSubmit(RidePreference newPreference) {
+//     Navigator.of(context).pop(newPreference);
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         body: Padding(
+//       padding: const EdgeInsets.only(
+//           left: BlaSpacings.m, right: BlaSpacings.m, top: BlaSpacings.s),
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: [
+//           // Back icon
+//           BlaIconButton(
+//             onPressed: onBackSelected,
+//             icon: Icons.close,
+//           ),
+//           SizedBox(height: BlaSpacings.m),
+
+//           // Title
+//           Text("Edit your search",
+//               style: BlaTextStyles.title.copyWith(color: BlaColors.textNormal)),
+
+//           // Form
+//           Expanded(
+//               child: Padding(
+//             padding: const EdgeInsets.all(10),
+//             child: RidePrefForm(
+//               initialPreference: widget.initialPreference,
+//               onSubmit: onSubmit,
+//             ),
+//           )),
+//         ],
+//       ),
+//     ));
+//   }
+// }
